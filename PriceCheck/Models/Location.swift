e939227526c73d05505980c63e6396726560cb27
Tishyaa Chaudhry <tishyaa.chaudhry@gmail.com>
2024-09-30 15:06:55 -0400

    Checkpoint 1

--- PriceCheck/Models/Location.swift
+++ PriceCheck/Models/Location.swift
@@ -11,26 +11,24 @@ struct Location: Identifiable, Codable, Comparable {
   
   var id = UUID()  // to conform to Identifiable protocol
   // Additional properties, etc.
+  var name: String
+  var scans: [PriceScan]
+    
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  enum CodingKeys: String, CodingKey {
+    case name = "location"
+    case scans
+  }
   
   // To conform to Comparable protocol
   static func < (lhs: Location, rhs: Location) -> Bool {
-    
+    lhs.name < rhs.name
   }
   
   static func == (lhs: Location, rhs: Location) -> Bool {
-    
+    lhs.name == rhs.name
   }
   
   // MARK: Example for SwiftUI
-  
+  static let example = Location(id: UUID(), name: "Target", scans: [PriceScan.example])
 }
--- PriceCheck/Models/PriceScan.swift
+++ PriceCheck/Models/PriceScan.swift
@@ -11,26 +11,27 @@ struct PriceScan: Identifiable, Codable, Comparable {
   
   var id = UUID()  // To conform to Identifiable protocol
   // Additional properties, etc.
+  var item: String
+  var date: String
+  var price: Double
   
-
+  enum CodingKeys: String, CodingKey {
+    case item
+    case date
+    case price
+  }
   
-  
-  
-  
-  
-  
-  
-  
-//  func onDate() -> String {
-//    // return Helper.getShortDate(       )
-//  }
+  func onDate() -> String {
+    return Helper.getShortDate(self.date)
+  }
   
   // To conform to Comparable protocol
   static func < (lhs: PriceScan, rhs: PriceScan) -> Bool {
-    
+    lhs.item < rhs.item
   }
   
   // MARK: Example for SwiftUI
-  
+  static let example = PriceScan(id: UUID(), item: "Charmin Soft 12 Mega Rolls", date: "2020-10-10T16:48:00", price: 14.79)
+
 }
 
